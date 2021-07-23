f9240f91d0db1784a294d447535f3e6cd0b1f672
Olegroom <rumyancev.olezhka@gmail.com>
2021-07-23 16:05:41 +0300

    0.0

---
+++ 1_module/ex00/megaphone.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cstring>
+
+int		main(int ac, char **av)
+{
+	int i;
+
+	i = 0;
+	std::string str;
+
+	if (ac == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	else
+		while (++i < ac)
+			str += av[i];
+	for (i = 0; i < str.length(); ++i)
+		putchar(toupper(str[i]));
+	std::cout << std::endl;
+	return (0);
+
+}
