12bc065b9734b250ea6e1f88caf0be166165e457
Olegroom <rumyancev.olezhka@gmail.com>
2021-07-27 22:54:10 +0300

    02,ex00

---
+++ 3_module/ex00/Fixed.cpp
@@ -0,0 +1,34 @@
+#include "Fixed.hpp"
+#include <string>
+#include <iostream>
+
+Fixed::Fixed(){
+	std::cout << "Default constructor called" << std::endl;
+	this->fixed_p_value = 0;
+}
+
+Fixed::~Fixed(){
+	std::cout << "Destructor called" << std::endl;
+}
+
+
+void Fixed::operator = (const Fixed &f2) {
+	std::cout << "Assignation operator called" << std::endl;
+	this->fixed_p_value = f2.getRawBits();
+}
+
+Fixed::Fixed(const Fixed &f2) {
+	std::cout << "Copy constructor called" << std::endl;
+	*this = f2;
+}
+
+int	Fixed::getRawBits(void) const {
+	std::cout << "getRawBits member function called" << std::endl;
+	return (this->fixed_p_value);
+}
+
+void	Fixed::setRawBits( int const raw_bits) {
+	this->fixed_p_value = raw_bits;
+}
+
+
---
+++ 3_module/ex01/Fixed.hpp
@@ -0,0 +1,18 @@
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+class Fixed
+{
+private:
+	int fixed_p_value;
+	const static int numb_frac_bits = 8;
+public:
+	Fixed();
+	Fixed(const Fixed &);
+	void operator = (const Fixed &);
+	~Fixed();
+	int getRawBits( void ) const;
+	void setRawBits( int const );
+};
+
+#endif
